b81d872ce5fd37bf53266f532d899c970be96b92
CristianCavanzo <cristian.frontendev@gmail.com>
2026-02-01 05:04:01 -0500

    [Add]: prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-function Page() {
+async function Page() {
     return (
         <div className="min-h-screen min-w-screen flex items-center justify-center">
             <Button className={cn('bg-blue-500 text-white hover:bg-blue-600')}>
